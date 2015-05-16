1b97e090d6cb930c8e3c433b7c38574be22bd87c
LauraKirby <laurakirby26@gmail.com>
2015-05-15 23:41:50 -0700

    add functions to script for original state and click event

--- matching.js
+++ matching.js
@@ -1,110 +1,52 @@
 var body = document.getElementsByTagName('body')[0];
 var graphDiv; 
-//var graphFunc = graph(); 
-// var flipFunc = flip(); 
+var timeoutID;
+//var image = ("https://farm9.staticflickr.com/8608/16237423798_dc48d24514_b.jpg"); 
+var newColor = "yellow"; 
+var classNameGreen = "originalState";
 
-//function graph (){
-// for(var i = 0; i < 9; i++){
-//     graphDiv = document.createElement('DIV');
-//     body.appendChild(graphDiv);
-//   //  graphDiv.className = "laura";
-//     //graphDiv.style.backgroundColor = "green"
-//     //this.className = "";
-//    // graphDiv.style.backgroundColor = "green"; 
-//     graphDiv.style.width = "25%";
-//     graphDiv.style.float = "left";
-//     graphDiv.style.border = "1px solid black";
-//     graphDiv.style.paddingBottom = "25%";
-//   }
-
-//     graphDiv.addEventListener("click", function(){
-        
-//        this.className = "laura"; 
-//        //this.style.backgroundColor = "green";
-//        console.log("bam"); 
-
-// }
-
-
-
-//     graphDiv.addEventListener("click", function(){
-//  //      this.className = ""; 
-//        this.className = "laura"
-//        //this.style.backgroundColor = "green";
-//        console.log("bam")
-//     })
-
-// }
-
-
-
-
-
-for(var i = 0; i < 9; i++){
+function ogState (){
+  for(var i = 0; i < 9; i++){
     graphDiv = document.createElement('DIV');
     body.appendChild(graphDiv);
-  //  graphDiv.className = "laura";
-    graphDiv.style.backgroundColor = "green"; 
-    graphDiv.style.width = "25%";
-    graphDiv.style.float = "left";
-    graphDiv.style.border = "1px solid black";
-    graphDiv.style.paddingBottom = "25%";
-    
-    graphDiv.addEventListener("click", function(){
- //      this.className = ""; 
-       this.className = "laura"
-      // this.style.backgroundColor = "green";
-       console.log("bam")
-    })
-  }
+    graphDiv.className = classNameGreen; 
+    graphDiv.style.backgroundColor = changeToNewColor(); 
+  } 
+};
+ogState(); 
 
+function changeToNewColor () {
+  graphDiv.addEventListener("click", function(){
+       this.style.backgroundColor = newColor;
+    });                    
+}
 
-// for (var j = 0; j < 9; j ++)
+// function changeToImage () {
+//   graphDiv.addEventListener("click", function(){
+//       // this.className = "laura";
+//        this.style.backgroundColor = originalColor;
+//     //   this.style.backgroundColor = ;
+//       //  function switchToGreen() {
+//       //  var timeoutID = graphDiv.setTimeout(slowAlert, 2000);
+//       // }
+//     }
+// };
 
-
-// var colors = ["red", "orange", "yellow", "green", "blue"];
-// colors.forEach(function(anything){
-//     var div = document.createElement("DIV");
-//     body.appendChild(div);
-//     div.style.width = "200px";
-//     div.style.border = "1px solid black";
-//     div.style.backgroundColor = anything;
-//     div.style.paddingBottom = "10%"
-
-//     div.addEventListener("click", function(){
-//         color = this.style.backgroundColor;
+//  var flipToImage = function ( ) {
+//   graphDiv.addEventListener("click", function(){
+//       // this.className = "laura";
+//        this.backgroundColor = originalColor;
+//     //   this.style.backgroundColor = ;
+//       //  function switchToGreen() {
+//       //  var timeoutID = graphDiv.setTimeout(slowAlert, 2000);
+//       // }
 //     });
-// })
-// var body = document.getElementsByTagName('body')[0];
-// var newDiv;
-// var color = "green";
-// //var imageArray = ["http://images.clipartpanda.com/baby-forest-animal-clipart--9aTq7rcMy.png"]
+// }()();
 
-// for(var i = 0; i < 9; i++){
-//     newDiv = document.createElement('DIV');
-//     body.appendChild(newDiv);
-//     newDiv.style.backgroundColor = color;
-//     newDiv.style.width = "25%";
-//     newDiv.style.float = "left"; 
-//     newDiv.style.border = "1px solid black";
-//     newDiv.style.paddingBottom = "18%";
-//     newDiv.addEventListener("click", function(){
-//         this.style.backgroundColor = yellow;
-//     })
+//flipToImage(); 
+
+
+// function switchToGreen() {
+//   var timeoutID  = window.setTimeout(code, [delay]);
+//   timeoutID = window.setTimeout(slowAlert, 2000);
 // }
-
-
-
-// var colors = ["red", "orange", "yellow", "green", "blue"];
-// colors.forEach(function(anything){
-//     var div = document.createElement("DIV");
-//     body.appendChild(div);
-//     div.style.width = "200px";
-//     div.style.border = "1px solid black";
-//     div.style.backgroundColor = anything;
-//     div.style.paddingBottom = "10%"
-
-//     div.addEventListener("click", function(){
-//         color = this.style.backgroundColor;
-//     });
-// })
